776c28a0cad4db975e8ee7db537c5afa6a8cb06e
jhongertf <jhongertf@gmail.com>
2019-07-14 22:05:38 -0400

    Adding loading overlay

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const imgContainer = document.getElementById('images'),
-      input = document.getElementById('term')
+      input = document.getElementById('term'),
+      loader = document.getElementById('loader');
 
 /**
  * Function createElementes
@@ -7,10 +8,14 @@ const imgContainer = document.getElementById('images'),
  * Create an img elemente for each image in the array and append it to images div
  */
 const createElements = images => {
+    // Loop the images array
     images.forEach( image => {
+
+        // Create an img element for each image
         let img = document.createElement('img');
         img.setAttribute('src', image.urls.full)
-    
+        
+        // Append the image to the container
         imgContainer.appendChild(img)
     })
 }
@@ -21,15 +26,19 @@ const createElements = images => {
  * Make http request to unsplash API
  */
 const makeRequest = term => {
+    // Show the loading overlay
+    loader.style.display = 'block';
 
     let httpRequest = new XMLHttpRequest();
 
     if (!httpRequest) {
+        loader.style.display = 'none';
         alert('Cannot create an XMLHTTP instance');
         return false;
     }
 
-    const queryUrl = 'https://api.unsplash.com/search/photos/?client_id=efd85f096277736ec45e1c6c627f9e9a2fe91f4532d1e9d330d6d80352111707&page=1&per_page=25&query=' + term;
+    // API url and parameters
+    const queryUrl = 'https://api.unsplash.com/search/photos/?client_id=efd85f096277736ec45e1c6c627f9e9a2fe91f4532d1e9d330d6d80352111707&page=1&per_page=5&query=' + term;
 
     httpRequest.open('GET', queryUrl);
     httpRequest.send();
@@ -37,8 +46,12 @@ const makeRequest = term => {
     httpRequest.onreadystatechange = e => {
         if (httpRequest.readyState === XMLHttpRequest.DONE){
             if(httpRequest.status === 200) {
-                createElements(JSON.parse(httpRequest.responseText.result));
+                imgContainer.innerHTML = '';
+                let data = JSON.parse(httpRequest.responseText);
+                createElements(data.results);
+                loader.style.display = 'none';
             } else {
+                loader.style.display = 'none';
                 alert('There was a problem processing the request.');
             }
         }
